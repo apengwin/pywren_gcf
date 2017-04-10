2ba7ca501ba58a37231318452f412c3287b00d8d
Allan Peng <potette007@gmail.com>
2017-04-10 13:53:36 -0700

    ugh

--- index.js
+++ index.js
@@ -24,24 +24,24 @@ function downLoadRuntime(bucketName, src, dest) {
     const bucket = storage.bucket(bucketName);
     const file = bucket.file(src);
 
-    try {
-        if (!fs.statSync(dest).isDirectory) {
-            throw "Dest is not a directory"
-        }
-    } catch(err) {
-        fs.mkdir(dest);
-    }
+//    try {
+  //      if (!fs.statSync(dest).isDirectory) {
+   //         throw "Dest is not a directory"
+ //       }
+//    } catch(err) {
+ //       fs.mkdir(dest);
+//    }
     const options = {
         // The path to which the file should be downloaded, e.g. "./file.txt"
-        destination: dest + "/" + src
+        destination:  "./" + src
     };
     console.log("Attempting to download runtime");
     return file.download(options)
      .then(() => {
          console.log(`File ${file.name} downloaded to ${dest}.`);
-         fs.createReadStream(dest + "/" + src).pipe(gunzip()).pipe(tar.extract(dest))
+         fs.createReadStream(dest + "/" + src).pipe(gunzip()).pipe(tar.extract(src))
             .on("finish", () => {
-              console.log(`Tarball extracted in ${dest}.`);
+              console.log(`Tarball extracted in ${src}.`);
             });
     });
 }
